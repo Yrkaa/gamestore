b406d02d5f615cf717d01e7191015384c0d98275
Yrka <varnack30@gmail.com>
2023-06-29 21:33:11 +0300

    create model Game

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.model):
+    name = models.CharField(blank=False, max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField(blank=False)
+    href = models.CharField(blank=False, max_length=200)

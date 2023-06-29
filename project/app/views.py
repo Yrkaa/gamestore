3684d25d5d7704342522c68160c2f2478e1cba0c
Yrka <varnack30@gmail.com>
2023-06-29 21:23:52 +0300

    create view for add_game page

--- project/app/views.py
+++ project/app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def add_game(request):
+    return render(request, 'add_game.html')
+    

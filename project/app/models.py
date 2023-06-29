4d875a6b8958506decb8ae135d1894231a3e3b65
Yrka <varnack30@gmail.com>
2023-06-29 21:36:12 +0300

    update model Game

--- project/app/models.py
+++ project/app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Game(models.model):
+class Game(models.Model):
     name = models.CharField(blank=False, max_length=100)
     description = models.TextField()
     price = models.PositiveIntegerField(blank=False)

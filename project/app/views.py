88f11399ee8c013eccf39051538e6edfa3c08900
Yrka <varnack30@gmail.com>
2023-06-30 12:18:25 +0300

    get forms

--- project/app/views.py
+++ project/app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from app.models import Game
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def add_game(request):
+    name = request.POST.get('name')
+    description = request.POST.get('description')
+    price = request.POST.get('price')
+    href = request.POST.get('href')
     return render(request, 'add_game.html')
     
